535455b96f2978c051481107a1bf7046ed44a453
Jean Carlos Vitola Cabarcas <jeanvitola@gmail.com>
2022-08-19 20:46:17 +0000

    main

--- testdataframe.py
+++ testdataframe.py
@@ -26,7 +26,8 @@ def get_connection():
         print("Connection could not be made due to the following error: \n", ex)
 
 def  tablaPrincipal():
-    tabla_principal=pd.concat(conjunto)
+    tabla=pd.concat(conjunto)
+    tabla_principal=tabla
     tabla_principal["id"]=tabla_principal.index 
     tabla_principal.reset_index(drop=True, inplace=True)
     tabla_principal.set_index("id", inplace=True)
@@ -35,6 +36,47 @@ def  tablaPrincipal():
 
     try:
         tabla_principal.to_sql('tabla_principal', url, if_exists='replace')
+
+    except:
+        print("no se subió a la BBDD")
+
+    #Tabla de categoria y cantidad
+    categ_cant= tabla_principal.groupby('categoria', as_index=False).size()
+    categ_cant=categ_cant.rename(columns={'size':'cantidad'})
+    categ_cant["date"]=datetime.date.today().strftime("%Y-%m-%d")
+    categ_cant
+    try:
+        categ_cant.to_sql('tablaCategoria', url, if_exists='replace')
+
+    except:
+        print("no se subió a la BBDD")
+
+
+    # Tabla de cantidad por fuente
+ 
+    fuente=list()
+    for name, df in tabla.items():
+        fuente.append({'fuente':name,'cantidad':df.size,})
+
+    tabla_fuente=pd.DataFrame(fuente)
+    tabla_fuente["date"]=datetime.date.today().strftime("%Y-%m-%d")
+    tabla_fuente
+
+    try:
+        categ_cant.to_sql('tablaCategoria', url, if_exists='replace')
+
+    except:
+        print("no se subió a la BBDD")
+
+  #Tabla de cantidad, proviencia y fuente
+  
+    provi_cant=tabla_principal.groupby(['categoria','provincia'], as_index=False).size()
+    provi_cant=provi_cant.rename(columns={'size':'cantidad'})
+    provi_cant["date"]=datetime.date.today().strftime("%Y-%m-%d")
+    provi_cant
+
+    try:
+        categ_cant.to_sql('tablaCategoria', url, if_exists='replace')
 
     except:
         print("no se subió a la BBDD")
